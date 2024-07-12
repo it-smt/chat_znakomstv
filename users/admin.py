c61e6481566ed7bdde15d38bc3e8ff1d68f790e5
smt-IT <smt_razrab@mail.ru>
2024-07-12 20:42:31 +0500

    Добавлен класс жалобы

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Photo, Video
+from .models import User, Photo, Video, Complaint
 
 
 class PhotoInline(admin.TabularInline):
@@ -17,3 +17,11 @@ class UserAdmin(admin.ModelAdmin):
     """Представление пользователя в админке."""
     list_display = ['id', 'user_id', 'username', 'first_name', 'last_name', 'age', 'city']
     inlines = [PhotoInline, VideoInline]
+
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    """Представление жалобы в админке."""
+    list_display = ['id', 'user', 'accused_user', 'date_created']
+    list_filter = ['date_created', 'user__age', 'user__city', 'user__gender', 'accused_user__age', 'accused_user__city', 'accused_user__gender']
+    search_fields = ['user__username', 'user__user_id', 'user__id', 'accused_user__username', 'accused_user__user_id', 'accused_user__id', 'description']
